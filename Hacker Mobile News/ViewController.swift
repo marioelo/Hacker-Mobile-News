02beaae118f5a2d24d88d6563a8c82072bc76e66
Mario Elorza <mario.elorza@gmail.com>
2021-05-06 22:19:15 -0400

    Initial Commit

---
+++ Hacker Mobile News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hacker Mobile News
+//
+//  Created by Mario Elorza on 06-05-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
